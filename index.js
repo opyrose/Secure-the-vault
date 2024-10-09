956ed53b921d6ae5c72095a254b006ac7d786b0a
Your Name <you@example.com>
2024-10-09 13:46:25 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+//create three variables 
+//each variable uses an artithmetic equation unique to it (+, -, /)
+// the final values of the variables must be 10, 40, 39
+// add alert
+// alert has a data type of string
+// string message states to user:
+// " You have received this message because you have been 
+//chosen to open an important vault. 
+//Here is the secret combination: 10-30-49"
+
+const variableOne= 100 / 10
+//console.log(variableOne)
+
+const variableTwo= 25 + 15
+//console.log(variableTwo)
+
+const variableThree= 53 - 14
+//console.log(variableThree)
+
+alert (` You have received this message because you have been chosen to open an important vault. Here is the secret combination: 10-40-39`);
